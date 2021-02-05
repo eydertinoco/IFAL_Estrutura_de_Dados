cbbe6a54ecaac31509b1b5f96dd466f2946d7315
Eyder <eydertinoco@outlook.com>
2021-02-05 17:03:13 -0300

    Primeira Questão da Lista 3 resolvida.

--- RevisaoPython3/1Q.py
+++ RevisaoPython3/1Q.py
@@ -1,63 +1,60 @@
 from typing import List
 from copy import deepcopy
-
 import none as none
 
-
-def adquirirValor(x, stack):
-    valor1 = int(stack.pop(x-1))
-    valor2 = int(stack.pop(x-2))
-    return valor1, valor2
+def adquirirValor(stack, tamanhoStack, x, equacao):
+    valor1 = float(stack.pop())
+    tamanhoStack -= 1
+    valor2 = float(stack.pop())
+    tamanhoStack -= 1
+    if equacao[x] == "+":
+        stack.append(float(valor2+valor1))
+    elif equacao[x] == "-":
+        stack.append(float(valor2-valor1))
+    elif equacao[x] == "*":
+        stack.append(float(valor2*valor1))
+    else:
+        stack.append(float(valor2/valor1))
 
 print("Primeira Questão:\nAdicione uma Notação Polonesa Inversa(RPN) para ser calculada.")
 equacao = str(input("Adicione a equação: "))
-
+print(equacao)
 stack: List[str] = []
 
 x = 0
-valorStack = none
+tamanhoStack = 0
+valor1 = float(0)
+valor2 = float(0)
+InformacaoParaAdicionarPilha = str("")
 tamanho = len(equacao)
+print("O tamanho da equação é ", tamanho)
 while x < tamanho:
-    if equacao[x] == " ":
-        valorStack = none
-    elif x == "+":
-        adquirirValor(x, stack)
-    elif x == "-":
-    elif x == "/":
-    elif x == "*":
-
-    elif equacao[x] != " " and equacao[x + 1] == " ":
-        valorStack += equacao[x]
-        stack.append(valorStack)
-        valorStack = none
-    else:
-
+    if (equacao[x] == str("+")):
+        adquirirValor(stack, tamanhoStack, x, equacao)
+        tamanhoStack -= 1
+        print(stack)
+        print(tamanhoStack)
+    elif (equacao[x] == str("-")):
+        adquirirValor(stack, tamanhoStack, x, equacao)
+        tamanhoStack -= 1
+        print(stack)
+        print(tamanhoStack)
+    elif (equacao[x] == str("*")):
+        adquirirValor(stack, tamanhoStack, x, equacao)
+        tamanhoStack -= 1
+        print(stack)
+        print(tamanhoStack)
+    elif (equacao[x] == str("/")):
+        adquirirValor(stack, tamanhoStack, x, equacao)
+        tamanhoStack -= 1
+        print(stack)
+        print(tamanhoStack)
+    elif (equacao[x] != str(" ")):
+        InformacaoParaAdicionarPilha += equacao[x]
+    elif (equacao[x] == str(" ")):
+        if InformacaoParaAdicionarPilha != "":
+            stack.append(InformacaoParaAdicionarPilha)
+            tamanhoStack += 1
+            print(stack)
+            InformacaoParaAdicionarPilha = str("")
     x += 1
-stack.append('A')
-stack.append('B')
-stack.append('C')
-
-x = 0
-while x < len(equacao):
-    stack.append(equacao[x])
-    if x == "+":
-        adquirirValor(stack)
-        stack.append(( valor1 + valor2 ))
-    elif x == "-":
-        valor1 = int(stack.pop(x - 1))
-        valor2 = int(stack.pop(x - 2))
-        stack.append((valor1 - valor2))
-    elif x == "/":
-        valor1 = int(stack.pop(x - 1))
-        valor2 = int(stack.pop(x - 2))
-        stack.append((valor1 / valor2))
-    elif x == "*":
-        valor1 = int(stack.pop(x - 1))
-        valor2 = int(stack.pop(x - 2))
-        stack.append((valor1 * valor2))
-    else:
-        stack.append(equacao[x])
-
-top_item = stack.pop() #Retorna o valor do topo de pilha.
-
-print(stack, top_item)
