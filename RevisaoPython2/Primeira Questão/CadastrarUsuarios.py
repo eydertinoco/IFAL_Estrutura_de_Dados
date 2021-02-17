2cf965ab96d0d385d6784db11ff9d1009a69e1df
Eyder <eydertinoco@outlook.com>
2021-02-17 17:01:24 -0300

    Renomeado Pastas e tentativas da primeira questao

--- RevisaoPython2/Primeira Questão/CadastrarUsuarios.py
+++
@@ -1,8 +0,0 @@
-class CadastrarCliente:
-
-    def __init__(self, nomeCliente, cpfCliente, ruaCliente, cepCliente, cidadeCliente):
-        self.nomeCliente = nomeCliente
-        self.cpfCliente = cpfCliente
-        self.ruaCliente = ruaCliente
-        self.cepCliente = cepCliente
-        self.cidadeCliente = cidadeCliente
