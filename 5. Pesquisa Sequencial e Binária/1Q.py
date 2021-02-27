fe608be0c64ba56440d345f7177ee20c99a9d805
Eyder <eydertinoco@outlook.com>
2021-02-27 13:19:36 -0300

    Lista 5 Primeira Questão

--- 5. Pesquisa Sequencial e Binária/1Q.py
+++ 5. Pesquisa Sequencial e Binária/1Q.py
@@ -1,33 +1,15 @@
-x = int(input("Adicione um valor positivo: "))
+x = int(input("Adicione um valor inteiro positivo: "))
 y = int(0)
-media = int(0)
+media = float(x/2)
 
-# Pegando metade por metade até encontrar onde está a raiz. Ex o X = 30
-# 15*15 - 30 < 0,000001
-# 7,5*7,5 - 30 < 0,000001
-# 3,75*3,75 = 30 < 0,000001
-
-# 5,625*5,625 = 30 < 0,000001
-# 4.6875*4.6875 = 30 < 0,000001
-
-# 5.15625*5.15625 = 30 < 0,000001
-# 5.390625*5.390625 = 30 < 0,000001
-
-# 5,5078125*5,5078125 = 30 < 0,000001
-# 5,4492187*5,4492187 =
-
-
-while y < x:
-    if (y*y) == x:
-        print("A raiz de {} é igual a {}".format(x,y))
-    elif (y*y) > media and (y*y) < x:
-        media = (y*y)
-        print("A raiz de {} é próxima a {}".format(x, media))
-    elif y == (x/2)+1:
-        if (y*y) > media:
-            media = (y * y)
-    elif (y*y) > x:
+while (0 < x):
+    if ((media * media) - x) < 0.000001 and ((media * media) - x) > (-0.000001):
+        print("A raiz de {} é igual a {}".format(x, media))
         break;
-    y += 1
-
-
+    else:
+        if (media*media) < x:
+            y = media
+            media = (mediaAnterior+y)/2
+        else:
+            mediaAnterior = media
+            media = (media+y)/2
