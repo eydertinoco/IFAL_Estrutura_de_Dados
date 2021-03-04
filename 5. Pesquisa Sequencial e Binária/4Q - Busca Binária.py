c23b47279422e8341af45a3364a926bd59f2aa51
Eyder <eydertinoco@outlook.com>
2021-03-04 14:15:35 -0300

    Quarta Questão Concluida

--- 5. Pesquisa Sequencial e Binária/4Q - Busca Binária.py
+++ 5. Pesquisa Sequencial e Binária/4Q - Busca Binária.py
@@ -2,18 +2,35 @@ def pesquisa_binaria_valor(lista, i, r, valorDesejado):
     if r >= i:
         posicao = (i+r) // 2
         if lista[posicao] == valorDesejado:
-            return valorDesejado
+            return posicao
         elif lista[posicao] > valorDesejado:
             return pesquisa_binaria_valor(lista, i, posicao-1, valorDesejado)
         else:
             return pesquisa_binaria_valor(lista, posicao+1, r, valorDesejado)
     return -1
 
-addValorLista = int(input("Adicione uma quantidade da lista com um valor inteiro e positivo na lista: "))
-lista = list(range(0,addValorLista))
+def addValorLista():
+    adicionarValorLista = int(input("Adicione o {}° valor inteiro: ".format(len(lista))))
+    if (adicionarValorLista == -999):
+        print(lista)
+    else:
+        if len(lista) != 0:
+            if lista[(len(lista)-1)] != (adicionarValorLista-1):
+                print("Para realizar uma busca binária é necessário que os valores sejam sequencial.\nEx: 10, 11, 12, 13, 14\nTente novamente.")
+                return addValorLista()
+            else:
+                lista.append(adicionarValorLista)
+                return addValorLista()
+        else:
+            lista.append(adicionarValorLista)
+            return addValorLista()
+
+lista = []
+print("Adicione valores inteiros na lista, digite -999 para finalizar a adição de valores.")
+addValorLista()
 valorDesejado = int(input("Adicione o valor que deseja obter na lista: "))
 posicaoValor = pesquisa_binaria_valor(lista, 0, len(lista)-1, valorDesejado)
 if posicaoValor < 0:
-    print("O valor não existe na lista.")
+    print("O valor {} não existe na lista.".format(valorDesejado))
 else:
-    print("O valor existe, está na posição {}.".format(posicaoValor))
+    print("O valor {} existe, está na posição {}.".format(valorDesejado, posicaoValor))
